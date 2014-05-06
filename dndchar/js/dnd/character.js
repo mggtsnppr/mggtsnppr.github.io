36423a33675f92a47cbd122ce4aa0fb735bacd69
Filipp <htmlmasta@gmail.com>
2014-05-07 00:48:20 +0600

    Base character management   - Character model and collection   - Character list item template

--- dndchar/js/dnd/character.js
+++ dndchar/js/dnd/character.js
@@ -1,3 +1,27 @@
-var Character = Backbone.model.extend({
-  initialize: function() {}
+var Character = Backbone.Model.extend({
+  defaults: function() {
+    return {
+      name: 'Nameless',
+      state: 'Creation Step #1'
+    };
+  },
+  initialize: function() {
+    this.set(this.defaults());
+  }
+});
+
+var CharacterList = Backbone.Collection.extend({
+  model: Character,
+  localStorage: new Backbone.LocalStorage('dnd-characters')
+});
+
+var Characters = new CharacterList;
+
+var CharacterListItemView = Backbone.View.extend({
+  tagName: 'li',
+  template: _.template($('#character-list-item-template').html()),
+  render: function() {
+    this.$el.html(this.template(this.model.toJSON()));
+    return this;
+  }
 });
