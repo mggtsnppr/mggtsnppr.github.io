45b8f6777df0ffb4c73ce2ad09fe3613cdb57bf7
Filipp Dernovoy <mggtsnppr@gmail.com>
2014-05-12 22:56:29 +0600

    backbone investigations

--- dndchar/js/dnd/app.js
+++ dndchar/js/dnd/app.js
@@ -1,27 +1,3 @@
-var App = Backbone.Model.extend({
-
-});
-
-var AppRouter = Backbone.Router.extend({
-  initialize: function(params) {
-    this.model = new App();
-  },
-  routes: {
-    "": 'charList',
-    "character/add": 'add',
-    "character/remove/:id": 'remove',
-    "character/:id": 'character'
-  },
-  charList: function() {
-    console.log('charlist');
-  },
-  character: function(id) {
-    console.log('character #' + id);
-  },
-  add: function() {
-    console.log('add character');
-  },
-  remove: function(id) {
-    console.log('remove character #' + id);
-  }
+var App = Backbone.View.extend({
+  el: 'body'
 });
--- dndchar/js/dnd/character.js
+++ dndchar/js/dnd/character.js
@@ -2,17 +2,19 @@ var Character = Backbone.Model.extend({
   defaults: function() {
     return {
       name: 'Nameless',
-      state: 'Creation Step #1'
+      state: 'Creation Step #1',
+      order: CharacterList.nextOrder()
     };
-  },
-  initialize: function() {
-    this.set(this.defaults());
   }
 });
 
 var CharacterList = Backbone.Collection.extend({
   model: Character,
-  localStorage: new Backbone.LocalStorage('dnd-characters')
+  localStorage: new Backbone.LocalStorage('dnd-characters'),
+  nextOrder: function() {
+    if (!this.length) return 1;
+    return this.last().get('order') + 1;
+  }
 });
 
 var Characters = new CharacterList;
@@ -20,8 +22,25 @@ var Characters = new CharacterList;
 var CharacterListItemView = Backbone.View.extend({
   tagName: 'li',
   template: _.template($('#character-list-item-template').html()),
+  events: {
+    'dblclick': 'process'
+  },
   render: function() {
     this.$el.html(this.template(this.model.toJSON()));
     return this;
+  },
+  process: function() {
+    alert(this.model.get('state'));
+  },
+  initialize: function() {
+    this.listenTo(this.model, 'change', this.render);
+  }
+});
+
+var CharacterListView = Backbone.View.extend({
+  tagName: 'ul',
+  template: _.template($('#character-list-template').html()),
+  render: function() {
+
   }
 });
